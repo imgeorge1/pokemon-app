860044eff94196c6efd51d3145bbc6096867ba28
User <ubitoz133@gmail.com>
2024-11-07 20:45:40 +0400

    created random route

--- index.js
+++ index.js
@@ -10,9 +10,28 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", { name: null, weight: null, typeNames: [], abilityNames: [], error: null });
+    res.render("index.ejs", { name: null, weight: null, typeNames: [], abilityNames: [], error: null, weightR: null, typeNamesR: [], abilityNamesR: [], nameR: null, });
 });
+app.post("/random", async (req, res) => {
+    const randInt = Math.floor(Math.random() * 1302)
+    console.log(randInt)
+    try {
+        const response = await axios.get(API_URL + randInt);
+        const nameR = response.data.name;
+        const weightR = response.data.weight;
+        const typeNamesR = response.data.types.map(typeObj => typeObj.type.name);
+        const abilityNamesR = response.data.abilities.map(abilityObj => abilityObj.ability.name);
+        
+        res.render("index.ejs", { nameR, weightR, typeNamesR, abilityNamesR, error: null, name: null, weight: null, typeNames: [], abilityNames: [], });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: null, name: null, weight: null, typeNames: [], abilityNames: [],
+            error: "Failed to fetch data: " + error.message,
 
+        });
+    }
+})
 app.post("/submit", async (req, res) => {
     let pokemon = req.body["pName"];
     try {
@@ -22,15 +41,13 @@ app.post("/submit", async (req, res) => {
         const typeNames = response.data.types.map(typeObj => typeObj.type.name);
         const abilityNames = response.data.abilities.map(abilityObj => abilityObj.ability.name);
         
-        res.render("index.ejs", { name, weight, typeNames, abilityNames, error: null });
+        res.render("index.ejs", { name, weight, typeNames, abilityNames, error: null, weightR: null, typeNamesR: [], abilityNamesR: [], nameR: null, });
     } catch (error) {
         console.error("Failed to make request:", error.message);
         res.render("index.ejs", {
-            name: null,
-            weight: null,
-            typeNames: [],
-            abilityNames: [],
+            nameR, weightR, typeNamesR, abilityNamesR, error: null, name: null, weight: null, typeNames: [], abilityNames: [],
             error: "Failed to fetch data: " + error.message,
+
         });
     }
 });
